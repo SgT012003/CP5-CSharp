0065675725efa0d6535f3b14df8ad85e18f84539
Vinicius Silva <106930436+vinirex@users.noreply.github.com>
2026-04-27 20:47:26 -0300

    Feat: Repository

--- GameStoreMVC/Interfaces/IUsuarioRepositorio.cs
+++ GameStoreMVC/Interfaces/IUsuarioRepositorio.cs
@@ -1,7 +1,11 @@
-﻿namespace GameStoreMVC.Interfaces
+﻿using GameStoreMVC.Models;
+ 
+namespace GameStoreMVC.Interfaces
 {
     public interface IUsuarioRepositorio
     {
-   
+        void Adicionar(Usuario usuario);
+        Usuario? ObterPorEmail(string email);
     }
 }
+ 
